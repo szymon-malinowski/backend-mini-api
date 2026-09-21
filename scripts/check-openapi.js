c7cdfcb8ade710ae659b5b8811167a68765fba3d
Szymon Malinowski <szmalinowski2014@gmail.com>
2026-09-21 14:03:12 +0200

    fresh start

--- scripts/check-openapi.js
+++
@@ -1,4 +0,0 @@
-import SwaggerParser from '@apidevtools/swagger-parser';
-
-const api = await SwaggerParser.validate('docs/openapi.yaml');
-console.log(`Validated ${api.info.title}: ${Object.keys(api.paths).length} paths.`);
--- src/db/pool.js
+++
@@ -1,5 +0,0 @@
-import pg from 'pg';
-
-export function createPool(connectionString) {
-  return new pg.Pool({ connectionString, max: 10, connectionTimeoutMillis: 5000, statement_timeout: 10000 });
-}
